35ccaadbecf099c177ae82ce2933f68d1bbdbfb7
Lucas Figueroa <lucas_figueroa_11@hotmail.com>
2023-07-17 01:27:24 -0300

    agregamos react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
 import Titulos from "./componets/Titulos";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <>
+    <Container>
       <Titulos></Titulos>
-    </>
+    </Container>
   );
 }
 
--- src/componets/Titulos.jsx
+++ src/componets/Titulos.jsx
@@ -2,9 +2,10 @@ const Titulos = () => {
   //aqui escribimos la logica
   return (
     //aqui tambien se puede escribir la logica
-    <section>
-      <h1>Proyecto contador con React</h1>
-      <h2>este es un subtitulo</h2>
+    <section className="text-center">
+      <h1 className="display-4">Proyecto contador con React</h1>
+      <h2 className="display-6">este es un subtitulo</h2>
+      <hr />
     </section>
   );
 };
