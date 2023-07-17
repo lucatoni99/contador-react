0474536f9af0b5d87069cca0f7caaa77d3a7f8a3
Lucas Figueroa <lucas_figueroa_11@hotmail.com>
2023-07-17 01:40:20 -0300

    terminamos la clase

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import Titulos from "./componets/Titulos";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Contador from "./componets/contador";
 
 function App() {
   return (
     <Container>
       <Titulos></Titulos>
+      <Contador></Contador>
     </Container>
   );
 }
